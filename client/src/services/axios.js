d5830c7e66f176f14ceab6cebe016d321b1ad9e7
vishal meshram <107854847+vishalboudhh@users.noreply.github.com>
2025-07-08 16:18:54 +0530

    Update axios.js

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // your backend URL
+  baseURL: 'https://fitnesstracker-218w.onrender.com', // your backend URL
 });
 
 instance.interceptors.request.use((config) => {
